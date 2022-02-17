73712207d15ee1a06cfe21a3d05662bbff04c507
marrk85 <96385065+marrk85@users.noreply.github.com>
2022-02-17 08:08:37 +0530

    Update utils.py

--- driver/utils.py
+++ driver/utils.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-from driver.jennie import bot, call_py
+from driver.marrk import bot, call_py
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from driver.queues import QUEUE, clear_queue, get_queue, pop_an_item
